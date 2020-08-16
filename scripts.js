eff14523274f9dadfae5400f0a4a39ba7f3a80fc
Elsee Sea <sub01+antix@iaonline.net>
2020-08-16 01:28:24 -0500

    More changes. Move right & move left are still broken.

--- scripts.js
+++ scripts.js
@@ -6,6 +6,11 @@ let bntMissionAbort = null;
 let pFlightStatus = null;
 let bkgShuttle = null;
 let shuttleHeight = null;
+let imgRocket = null;
+let btnDown = null;
+let btnUp = null;
+let btnLeft = null;
+let btnRight = null;
 
 function init () {
     btnTakeOff = document.getElementById("takeoff");
@@ -14,6 +19,12 @@ function init () {
     pFlightStatus = document.getElementById("flightStatus");
     bkgShuttle = document.getElementById("shuttleBackground");
 	shuttleHeight = document.getElementById("spaceShuttleHeight");
+	btnDown = document.getElementById("down");
+	btnUp = document.getElementById("up");
+	btnLeft = document.getElementById("left");
+	btnRight = document.getElementById("right");
+	imgRocket = document.getElementById('rocket')
+	imgRocket.style.position = 'relative';
 /*
  * Take off
  */
@@ -46,19 +57,57 @@ function init () {
     btnMissionAbort.addEventListener("click", function (event) {
         let confirmMissionAbort = window.confirm("Confirm that you want to abort the mission");
         if (confirmMissionAbort) {
-			pFlightStatus.innerHTML = "Shuttle in flight";
-			bkgShuttle.style.backgroundColor = "blue";
-			shuttleHeight.innerHTML = "10000";
+			pFlightStatus.innerHTML = "Mission aborted";
+			bkgShuttle.style.backgroundColor = "green";
+			shuttleHeight.innerHTML = "0";
 		};
     });
- 
-    /*
-    3. When the user clicks the "Abort Mission" button, make a confirmation window that says "Are you sure you want to abort the mission?".
-    */
 
-    missionAbort.addEventListener("click", function (event) {
-      window.confirm("Are you sure you want to abort the mission?");
-    });
+function getX() {
+	return (imgRocket.offsetLeft);
+}
+
+function getY() {
+	return (imgRocket.offsetTop);
+}
+
+/*
+ * Move down
+ */
+	btnDown.addEventListener("click", function (event) {
+		let imgY = getY();
+		imgRocket.style.top = imgY + 10 + 'px';
+	});
+ 
+/*
+ * Move up
+ */
+	btnUp.addEventListener("click", function (event) {
+		let imgY = parseInt(getY());
+		imgY = imgY - 10;
+		imgRocket.style.top = imgY + 'px';
+	});
+ 
+/*
+ * Move left
+ */
+	btnLeft.addEventListener("click", function (event) {
+		let imgX = parseInt(getX());
+		imgX -= 10;
+		imgRocket.style.left = imgX + 'px';
+	});
+ 
+/*
+ * Move right
+ */
+	btnRight.addEventListener("click", function (event) {
+		let imgX = getX();
+console.log('imgX = ' + imgX);
+let newLoc = imgX + 10;
+console.log('newLoc = ' + newLoc);
+		imgRocket.style.left = newLoc;
+	});
+ 
 }
 
 window.onload = init;
